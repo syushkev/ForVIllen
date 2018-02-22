20ff31589308e84300c2d6c4edf4937a47980255
Yurii Zherebukh <33144915+YZherebukh@users.noreply.github.com>
2018-02-22 17:41:37 +0200

    Update main.go

--- main/main.go
+++ main/main.go
@@ -8,30 +8,17 @@ import (
 
 const defaultREconstant  = `(\d+)\.(.+)`
 
-func parceFlags(version, expression string)  (version1, expression1 string){
-	f := flag.NewFlagSet("flag", flag.ExitOnError)
-	versio := f.String("version", version, "Some description")
-	re := f.String("re", defaultREconstant, "Some description")
-
-	if expression == "" {
-		f.Parse([]string{"-re", defaultREconstant, "version", version})
-	}
-
-	if expression != ""{
-		f.Parse([]string{"-re",expression, "version", version})
-	}
-	return *versio, *re
-}
-
 func FindVersion123(version , expression string) (r []string, err error) {
 
-
 	req, err := regexp.Compile(expression)
-
 
 	if err != nil {
 		return nil, err
 	}
+	if version == ""{
+		return nil, errors.New("empty field version")
+	}
+
 	r = req.FindStringSubmatch(version)
 	if len(r) == 0 {
 		return nil, errors.New("invalid version")
@@ -42,17 +29,9 @@ func FindVersion123(version , expression string) (r []string, err error) {
 }
 
 func main() {
-	v,re := parceFlags("10.20.3.5", "")
-	one, err := FindVersion123(v, re)
+	versio := flag.String("version", "", "Some description")
+	re := flag.String("re", defaultREconstant, "Some description")
+	flag.Parse()
+	one, err := FindVersion123(*versio, *re)
 	fmt.Println(one, "", err)
-	fmt.Println()
-	v2,re2 := parceFlags("10.20.4.5", `(\d+\.\d+)\.(.+)`)
-	one2, err2 := FindVersion123(v2, re2)
-	fmt.Println(one2, "", err2)
-	fmt.Println()
-	v3,re3 := parceFlags("10.20.4.5.54.65.5.5.5.5.5.5.5.5.5", `(\d+\.\d+)\.(.+)`)
-	one3, err3 := FindVersion123(v3, re3)
-	fmt.Println(one3, "", err3)
-
 }
-
