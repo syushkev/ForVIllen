80fd0a4f3feb06400c66f7e68c3860f318645c24
Yurii Zherebukh <33144915+YZherebukh@users.noreply.github.com>
2018-02-22 15:46:28 +0200

    Update main.go

--- main/main.go
+++ main/main.go
@@ -6,16 +6,27 @@ import (
 	"errors"
 )
 
-func FindVersion123(version ...string) (r []string, err error) {
-	if len(version) == 1 {
-		expression := `(\d+)\.(.+)`
-		version = append(version, expression)
+const defaultREconstant  = `(\d+)\.(.+)`
+
+func FindVersion123(version , expression string) (r []string, err error) {
+	f := flag.NewFlagSet("flag", flag.ExitOnError)
+	versio := f.String("version", version, "Some description")
+	re := f.String("re", defaultREconstant, "Some description")
+
+	if expression == "" {
+	f.Parse([]string{defaultREconstant})
 	}
-	req, err := regexp.Compile(version[1])
+
+	if expression != ""{
+	f.Parse([]string{"-re",expression})
+	}
+	req, err := regexp.Compile(*re)
+
+
 	if err != nil {
 		return nil, err
 	}
-	r = req.FindStringSubmatch(version[0])
+	r = req.FindStringSubmatch(*versio)
 	if len(r) == 0 {
 		return nil, errors.New("invalid version")
 	}
@@ -25,12 +36,14 @@ func FindVersion123(version ...string) (r []string, err error) {
 }
 
 func main() {
-	//var validID = regexp.MustCompile(`(\d+\.\d+)\.(.+)`)
-	//var validID2 = regexp.MustCompile(`(\d+)\.(.+)`)
-	one, err := FindVersion123("10.20.3.5")
+
+	one, err := FindVersion123("10.20.3.5", "")
 	fmt.Println(one, "", err)
 	fmt.Println()
-	four, err := FindVersion123("10.20.3.5", `(\d+\.\d+)\.(.+)`)
+	four, err := FindVersion123("10.20.4.5", `(\d+\.\d+)\.(.+)`)
 	fmt.Println(four, "", err)
+	five, err := FindVersion123("10.20.4.5.54.65.5.5.5.5.5.5.5.5.5", `(\d+\.\d+)\.(.+)`)
+	fmt.Println(five, "", err)
 
 }
+
